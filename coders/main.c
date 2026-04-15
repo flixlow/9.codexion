baabefc02e1d0f51eebfb8a7c5a30c9c9e2f9036
Felix Lauwerie <flauweri@z2r3p1.42lyon.fr>
2026-04-15 14:37:32 +0200

    add: stderr output when an error occurs

--- coders/main.c
+++ coders/main.c
@@ -6,7 +6,7 @@
 /*   By: flauweri <flauweri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 11:13:53 by flauweri          #+#    #+#             */
-/*   Updated: 2026/04/15 13:32:00 by flauweri         ###   ########.fr       */
+/*   Updated: 2026/04/15 14:08:04 by flauweri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,17 +39,16 @@ int check_args(int ac, char **av)
 	i = 1;
 	if (ac != 9)
 	{
-		printf("Error: ./codexion <number_of_coders> <time_to_burnout> ");
-		printf("<time_to_compile> <time_to_debug> <time_to_refactor> ");
-		printf("<number_of_compiles_required> <dongle_cooldown> <scheduler>");
+		fprintf(stderr, "[ERROR] ./codexion <number_of_coders> ");
+		fprintf(stderr, "<time_to_burnout> <time_to_compile> <time_to_debug> ");
+		fprintf(stderr, "<time_to_refactor> <number_of_compiles_required> ");
+		fprintf(stderr, "<dongle_cooldown> <scheduler>");
 		return (1);
 	}
 	while (i <= 7)
 	{
 		if (check_arg(av[i]))
-		{
 			return (1);
-		}
 		i++;
 	}
 	if (!strcmp(av[8], "edf") || !strcmp(av[8], "fifo"))
